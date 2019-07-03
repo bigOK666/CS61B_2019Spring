b9b4ebc4de5e58c42633fb6ac23736892003b3bd
Lu <zeren.lu@gmail.com>
2019-07-03 22:42:55 +0200

    add judge function

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,17 @@ public class LeapYear {
         }
     }
 
+    /** Examine if a year is leap.
+     * @param year to be checked
+     */
+    public static boolean isLeapYear(int year){
+    	if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+    		return true;
+    	}else {
+    		return false;
+    	}
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
