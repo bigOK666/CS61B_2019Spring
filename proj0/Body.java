fb10708cced82ee988b4ff4461132a96c48988d9
Lu <zeren.lu@gmail.com>
2019-07-03 22:37:04 +0200

    body class for proj0

---
+++ proj0/Body.java
@@ -0,0 +1,17 @@
+public class Body{
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+
+	public Body(double xP, double yP, double xV, double yV, double m, String img){
+
+	}
+
+	public Body(Body b){
+		
+	}
+	
+}
