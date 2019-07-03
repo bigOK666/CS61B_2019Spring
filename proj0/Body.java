ca6e5d56417f6008164e7d6caf3b7db9584846c1
Lu <zeren.lu@gmail.com>
2019-07-03 22:52:09 +0200

    finish constructors

--- proj0/Body.java
+++ proj0/Body.java
@@ -7,11 +7,22 @@ public class Body{
 	public String imgFileName;
 
 	public Body(double xP, double yP, double xV, double yV, double m, String img){
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
 
 	}
 
 	public Body(Body b){
-		
+		xxPos = b.xxPos;
+		yyPos = b.yyPos;
+		xxVel = b.xxVel;
+		yyVel = b.yyVel;
+		mass = b.mass;
+		imgFileName = b.imgFileName;
 	}
 	
 }
