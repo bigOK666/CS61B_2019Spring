e22aa5db9ec832bccca65f274fb9a41567d73746
Lu <zeren.lu@gmail.com>
2019-07-03 22:33:41 +0200

    add hello files

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+		while (i<10){
+			System.out.println(sum);
+			i=i+1;
+			sum = sum + i;
+			
+
+		}
+		System.out.println(5 + "10");
+		System.out.println(5 + 10);
+	}
+}
