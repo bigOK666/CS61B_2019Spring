7461421918b5c4e8ea86fa9306e34b313cbded6f
Lu <zeren.lu@gmail.com>
2019-07-04 21:54:46 +0200

    calculate Force applied on each body

--- proj0/Body.java
+++ proj0/Body.java
@@ -5,6 +5,7 @@ public class Body{
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
+	public static double G = 6.67e-11;
 
 	public Body(double xP, double yP, double xV, double yV, double m, String img){
 		xxPos = xP;
@@ -29,4 +30,7 @@ public class Body{
 		return Math.sqrt((Math.pow((b.xxPos - this.xxPos), 2) + Math.pow((b.yyPos - this.yyPos), 2)));
 	}
 	
+	public double calcForceExertedBy(Body b){
+		return G * this.mass * b.mass / Math.pow(this.calcDistance(b), 2);
+	}
 }
