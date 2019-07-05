07bde0aa63ba36803539f31d986b92e0441eee72
Lu <zeren.lu@gmail.com>
2019-07-05 21:33:11 +0200

    read Radius method

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,13 @@
 public class NBody{
-	
+	public static double readRadius(String fileName){
+		In in = new In(fileName);
+		int N = in.readInt();
+		double R = in.readDouble();
+		return R;
+	}
+
+	public static String[] readBodies(String fileName){
+		In in = new In(fileName);
+		int N = in.readInt();
+	}
 }
