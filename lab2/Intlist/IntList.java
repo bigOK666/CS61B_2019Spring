ad514f2623b29588f3a03da1077b54fb93245c2b
Lu <zeren.lu@gmail.com>
2019-07-22 23:00:22 +0200

    destructive build to concatenate to List

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList C = A;
+        while(C.rest != null){
+            C = C.rest;
+        }
+        C.rest = B;
+        return A;
     }
 
     /**
