197722865c56d2018de4aed835236c7f72e81120
Lu <zeren.lu@gmail.com>
2020-04-23 14:29:04 +0200

    initial with an empty array

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,27 @@
-public class ArrayDeque {
+public class ArrayDeque<T> {
+    private int size, nextFirst, nextLast;
+    private T[] items;
+
+    public ArrayDeque() {
+        size = 0;
+        nextFirst = 0;
+        nextLast = 0;
+        items = (T []) new Object[8];
+    }
+    public ArrayDeque(ArrayDeque other) {
+
+    }
+
+    /*resize the array*/
+    private void resize(int capacity) {
+        int[] a = new int[capacity];
+
+    }
+
+    /*add item to the last*/
+    public void addLast(T item) {
+        items[nextLast] = item;
+        nextLast++;
+        size++;
+    }
 }
