f9f9f3dd79486152dd7eb69a65c59a53a516bd14
Lu <zeren.lu@gmail.com>
2020-05-08 23:24:12 +0200

    interface deque

---
+++ proj1b/Deque.java
@@ -0,0 +1,17 @@
+/*This interface defines the methods in all kinds of Deque*/
+
+public interface Deque<T> {
+    public void addFirst(T item);
+    public void addLast(T item);
+    public int size();
+    public void printDeque();
+    public T removeFirst();
+    public T removeLast();
+    public T get(int index);
+
+
+    default public boolean isEmpty() {
+        return size() == 0;
+    };
+
+}
