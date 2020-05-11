c3370ef0d263117aa9c7a93d8ca08cda87b0ffee
Lu <zeren.lu@gmail.com>
2020-05-11 22:05:07 +0200

    add implements

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,155 @@
+public class ArrayDeque<T> implements Deque<T>{
+    private int size, nextFirst, nextLast;
+    private T[] items;
+
+
+    public ArrayDeque() {
+        size = 0;
+        nextFirst = 7;
+        nextLast = 0;
+        items = (T []) new Object[8];
+    }
+    public ArrayDeque(ArrayDeque other) {
+        items = (T []) new Object[other.items.length];
+        for (int i = 0; i < other.items.length; i++) {
+            items[i] = (T) other.items[i];
+        }
+        size = other.size();
+        nextFirst = other.nextFirst();
+        nextLast = other.nextLast();
+    }
+
+    /*resize the array*/
+    private void resize(int capacity) {
+        T[] a = (T []) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, nextLast);
+        if (nextFirst + 1 > nextLast - 1) {
+            System.arraycopy(items, (nextFirst + 1) % items.length, a,
+                    (nextFirst + 1 + capacity - items.length) % capacity, size - nextLast);
+            nextFirst = nextFirst + capacity - items.length;
+        }
+
+        items = a;
+
+    }
+
+    /*add item to the last*/
+    public void addLast(T item) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        items[nextLast] = item;
+        nextLast++;
+        size++;
+    }
+
+    /*add item to the first*/
+    public void addFirst(T item) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        items[nextFirst] = item;
+        nextFirst = (nextFirst - 1 + items.length) % items.length;
+        size++;
+    }
+
+    /*check if array is empty*/
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /*show the size of the arraylist*/
+    public int size() {
+        return size;
+    }
+
+    public int nextFirst() {
+        return nextFirst;
+    }
+
+    public int nextLast() {
+        return nextLast;
+    }
+    /*show the item in the array list*/
+    public void printDeque() {
+        int L = items.length;
+        int firstIndex = (nextFirst + 1) % L;
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[(i + firstIndex) % L] + " ");
+        }
+        System.out.println(" ");
+    }
+
+    /*remove the first element*/
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            T x = items[(nextFirst + 1) % items.length];
+            items[(nextFirst + 1) % items.length] = null;
+            nextFirst = (nextFirst + 1) % items.length;
+            size--;
+            if (items.length >= 16 && (double) size / items.length < 0.25) {
+                resize(size * 2);
+            }
+            return x;
+        }
+    }
+
+    /*remove the last element*/
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            T x = items[nextLast - 1];
+            items[nextLast - 1] = null;
+            nextLast--;
+            size--;
+            if (items.length >= 16 && (double) size / items.length < 0.25) {
+                resize(size * 2);
+            }
+            return x;
+        }
+    }
+
+    /*get the ith element*/
+    public T get(int i) {
+        if (i > size - 1) {
+            return null;
+        } else {
+            return items[(i + nextFirst + 1) % items.length];
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> test = new ArrayDeque<>();
+        test.addFirst(1);
+        test.addLast(2);
+        test.addFirst(0);
+        test.addLast(3);
+        test.addLast(4);
+        test.addLast(5);
+        test.addLast(6);
+        test.addLast(7);
+        test.printDeque();
+        test.addLast(8);
+        test.removeFirst();
+        test.removeFirst();
+//        test.printDeque();
+        test.removeLast();
+        test.removeFirst();
+//        test.printDeque();
+//        System.out.print(test.get(2));
+        test.removeFirst();
+        test.removeLast();
+        System.out.print(test.get(2));
+
+        ArrayDeque<Integer> test2 = new ArrayDeque<>(test);
+        test2.removeLast();
+        test2.removeFirst();
+        test2.removeLast();
+        test2.addFirst(0);
+
+    }
+}
---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,145 @@
+public class LinkedListDeque<T> implements Deque<T> {
+    private Node sentinel;
+    private int size;
+    /* Create Node class to define a Node*/
+    private class Node {
+        private T item;
+        private Node pre, next;
+        private Node(T i, Node prv, Node nex) {
+            item = i;
+            pre = prv;
+            next = nex;
+        }
+    }
+    /* Create an empty linked list deque*/
+    public LinkedListDeque() {
+        sentinel = new Node(null, null, null);
+        sentinel.pre = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+    /* initial with one element*/
+    public LinkedListDeque(T x) {
+        sentinel = new Node(null, null, null);
+
+        sentinel.next = new Node(x, sentinel, sentinel);
+        sentinel.pre = sentinel.next;
+        size = 1;
+    }
+
+    /* add First element*/
+    public void addFirst(T item) {
+        Node node = new Node(item, sentinel, sentinel.next);
+        sentinel.next.pre = node;
+        sentinel.next = node;
+        size += 1;
+    }
+    /*Add element to last position*/
+    public void addLast(T item) {
+        Node node = new Node(item, sentinel.pre, sentinel);
+        sentinel.pre.next = node;
+        sentinel.pre = node;
+        size += 1;
+    }
+    /*check if list is empty*/
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    /*Return the number of elements in the list*/
+    public int size() {
+        return size;
+    }
+
+    /*print all the elements*/
+    public void printDeque() {
+        Node p = sentinel.next;
+        while (p.item != null) {
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+        System.out.println(" ");
+    }
+
+    /*remove first element*/
+    public T removeFirst() {
+        if (size != 0) {
+            T v = sentinel.next.item;
+            sentinel.next.next.pre = sentinel;
+            sentinel.next = sentinel.next.next;
+            size -= 1;
+            return v;
+        } else {
+            return null;
+        }
+
+    }
+
+    /*remove last element*/
+    public T removeLast() {
+        if (size != 0) {
+            T v = sentinel.pre.item;
+            sentinel.pre.pre.next = sentinel;
+            sentinel.pre = sentinel.pre.pre;
+            size -= 1;
+            return v;
+        } else {
+            return null;
+        }
+    }
+    /*return the element at given index*/
+    public T get(int index) {
+        if (index > size - 1 || index < 0) {
+            return null;
+        } else {
+            int counter = 0;
+            Node p = sentinel.next;
+            while (counter != index) {
+                p = p.next;
+                counter += 1;
+            }
+            return p.item;
+        }
+    }
+    /*copy the entire list object one by one.
+    * source @https://www.youtube.com/watch?v=JNroRiEG7U4
+    *
+    * */
+    public LinkedListDeque(LinkedListDeque other) {
+        sentinel = new Node(null, null, null);
+        sentinel.pre = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+
+        for (int i = 0; i < other.size; i++) {
+            addLast((T) other.get(i));
+        }
+
+    }
+    /*return the item based on index*/
+    public T getRecursive(int index) {
+        if (index > size - 1 || index < 0) {
+            return null;
+        } else {
+            return getRecursiveHelper(sentinel.next, index);
+        }
+    }
+
+    /*help function to get item based on index*/
+    private T getRecursiveHelper(Node n, int i) {
+        if (i == 0) {
+            return n.item;
+        } else {
+            return getRecursiveHelper(n.next, i - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListDeque<Integer> test = new LinkedListDeque<>(10);
+        test.addFirst(20);
+        test.addLast(30);
+//        test.printDeque();
+//        test.removeFirst();
+        test.removeLast();
+        System.out.print(test.getRecursive(1));
+    }
+}
