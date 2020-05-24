ff61ade9bc49bdbb9df6f7882615523591fd20e7
Lu <zeren.lu@gmail.com>
2020-05-24 10:13:24 +0200

    Palidrome not implemented yet

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,8 @@
+public class Palindrome {
+    /*Given a String, wordToDeque should return a Deque where the characters appear in the same order as in the String.
+    * For example, if the word is “persiflage”, then the returned Deque should have ‘p’ at the front, followed by ‘e’,
+    * and so forth*/
+    public Deque<Character> wordToDeque(String word){
+        return null;
+    }
+}
