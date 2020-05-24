62bdf707fa18fdb71616bcead23212d191f37168
Lu <zeren.lu@gmail.com>
2020-05-24 21:53:47 +0200

    test

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,8 +1,12 @@
-public class Palindrome {
+public class Palindrome extends ArrayDeque<Character>{
     /*Given a String, wordToDeque should return a Deque where the characters appear in the same order as in the String.
     * For example, if the word is “persiflage”, then the returned Deque should have ‘p’ at the front, followed by ‘e’,
     * and so forth*/
     public Deque<Character> wordToDeque(String word){
-        return null;
+        ArrayDeque<Character> palindrome_array = new ArrayDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            palindrome_array.addLast(word.charAt(i));
+        }
+        return palindrome_array;
     }
 }
