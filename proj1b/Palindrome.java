695ca37d05f52664968e3a6eec07dc07eeb9e290
Lu <zeren.lu@gmail.com>
2020-06-01 14:39:42 +0200

    dummy method to check if a string is Palindrome

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -2,11 +2,15 @@ public class Palindrome extends ArrayDeque<Character>{
     /*Given a String, wordToDeque should return a Deque where the characters appear in the same order as in the String.
     * For example, if the word is “persiflage”, then the returned Deque should have ‘p’ at the front, followed by ‘e’,
     * and so forth*/
-    public Deque<Character> wordToDeque(String word){
+    public Deque<Character> wordToDeque(String word) {
         ArrayDeque<Character> palindrome_array = new ArrayDeque<>();
         for (int i = 0; i < word.length(); i++) {
             palindrome_array.addLast(word.charAt(i));
         }
         return palindrome_array;
+    }
+
+    public boolean isPalindrome(String word) {
+        return false;
     }
 }
